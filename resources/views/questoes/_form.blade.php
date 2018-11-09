8c758f2f8672f4b61a5c86e9b1f668cfb7ccad42
dgobode <dgo.diegocarvalho@gmail.com>
2018-11-09 13:26:35 -0200

    telas de questoes e correcao no db para questoes

---
+++ resources/views/questoes/_form.blade.php
@@ -0,0 +1,9 @@
+<div class="t1">
+    <label><br />Descrição:<br /></label>
+    <input name="descQuestao" type="text" id="grande" value="{{ isset($registro->descQuestao) ? $registro->descQuestao : '' }}">
+</div>
+<br>
+<div class="btn">
+    <button class="btnCancel">Cancelar</button>
+    <button class="btnSave" >Salvar</button>
+</div>
--- resources/views/questoes/novo.blade.php
+++ resources/views/questoes/novo.blade.php
@@ -3,23 +3,11 @@
 @section('titulo','Cadastrar Questão')
 
 @section('conteudo')
-
-      <div class="conteudoForm">
-        <h3 class="center">Cadastrar Questão</h3>
-
-
-      <form class="" action="{{route('questoes.salvar')}}" method="post">
+<div class="conteudoForm">
+    <h3 class="center">Cadastrar Questão</h3>
+    <form class="" action="{{route('questoes.salvar')}}" method="post">
         {{ csrf_field() }}
-
-        
-
-         <br>
-        <div class="btn">
-          <button class="btnCancel">Cancelar</button>
-          <button class="btnSave" >Salvar</button>
-        </div>
-      </form>
-
-  </div>
-
+        @include('questoes._form')
+    </form>
+</div>
 @endsection
