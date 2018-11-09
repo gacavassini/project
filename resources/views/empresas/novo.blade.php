734946d650c58e0e5f89f025fc1ad2b733043b69
dgobode <dgo.diegocarvalho@gmail.com>
2018-11-09 12:32:53 -0200

    telas de empresas

--- resources/views/empresas/novo.blade.php
+++ resources/views/empresas/novo.blade.php
@@ -4,68 +4,13 @@
 
 @section('conteudo')
 
-      <div class="conteudoForm">
-    <h3 class="center">Cadastrar Empresa</h3>
+    <div class="conteudoForm">
+        <h3 class="center">Cadastrar Empresa</h3>
+        <form class="" action="{{route('empresas.salvar')}}" method="post">
+            {{ csrf_field() }}
+            @include('empresas._form')
+        </form>
 
-
-      <form class="" action="{{route('empresas.salvar')}}" method="post">
-        {{ csrf_field() }}
-
-        <div class="t1">
-          <label><br>Nome:<br> </label>
-          <input type="text" name="nome" id ="grande">
-        </div>
-
-        <div class="t2">
-          <label><br> CNPJ:<br> </label>
-          <input type="text" name="cnpj" id ="medio">
-        </div>
-
-        <div class="t1">
-          <label><br> Rua:<br> </label>
-          <input type="text" name="rua" id ="grande">
-        </div>
-
-        <div class="t3">
-          <label><br> Número:<br> </label>
-          <input type="text" name="numEndereco" id ="pequeno">
-        </div>
-
-        <div class="t2">
-          <label><br> Bairro:<br> </label>
-          <input type="text" name="bairro" id ="medio">
-        </div>     
-
-        
-        <div class="t2">
-          <label><br> Complemento:<br> </label>
-          <input type="text" name="complemento" id ="medio">
-        </div>   
-
-
-        <div class="t3">
-          <label><br> CEP:<br> </label>
-          <input type="text" name="cep" id ="pequeno">
-        </div>
-
-        <div class="t2">
-          <label><br> Cidade:<br> </label>
-          <input type="text" name="cidade" id ="medio">
-        </div>   
-
-
-        <div class="t2">
-          <label><br> Telefone:<br> </label>
-          <input type="text" name="telefone" id ="medio">
-        </div>
-
-         <br>
-        <div class="btn">
-          <button class="btnCancel">Cancelar</button>
-          <button class="btnSave" >Salvar</button>
-        </div>
-      </form>
-
-  </div>
+    </div>
 
 @endsection
