29a5e7959c800ee81b80ef50173bcf98b08847db
dgobode <dgo.diegocarvalho@gmail.com>
2018-11-09 13:08:50 -0200

    telas de pedidos

---
+++ resources/views/pedidos/_form.blade.php
@@ -0,0 +1,30 @@
+<div class="t3">
+    <div class="select-box">
+        <label><span><br>Tipo:</span><br></label>
+        <select id="pequeno" name="tipo" style="width: 68%; height:4vh" >
+            <option value=""></option>
+            <option value=true @if(isset($registro)) @if($registro->tipo == true) selected="selected" @endif @endif>Padrão</option>
+            <option value=false @if(isset($registro)) @if($registro->tipo == false) selected="selected" @endif @endif>Específico</option>
+        </select>
+    </div><!--Fechou select box do cliente -->
+</div>
+
+<div class="t1">
+    <label> <br>Nome:<br> </label>
+    <input name="nomePedido" id="grande" type="text" value="{{ isset($registro->nomePedido) ? $registro->nomePedido : '' }}">
+</div>
+
+<div class="t1">
+  <label> <br>Fundamentos:<br> </label>
+  <textarea id="grande" name="fundamento" cols="80" rows="7" style="height: 90%">{{ isset($registro->fundamento) ? $registro->fundamento : '' }}</textarea>
+</div>
+
+<div class="t1">
+  <label> <br>Resumo:<br> </label>
+  <textarea id="grande" name="resumo" cols="80" rows="7" style="height: 90%">{{ isset($registro->resumo) ? $registro->resumo : '' }}</textarea>
+</div>
+<br>
+<div class="btn">
+    <button class="btnCancel">Cancelar</button>
+    <button class="btnSave" >Salvar</button>
+</div>
