fdde2d970cc978e4ef0568f80a05a08ff366638d
dgobode <dgo.diegocarvalho@gmail.com>
2018-12-08 18:38:24 -0200

    inicio status no editar de atividades

--- resources/views/atividades/_form.blade.php
+++ resources/views/atividades/_form.blade.php
@@ -1,3 +1,68 @@
+<style>
+/* The switch - the box around the slider */
+.switch {
+position: relative;
+display: inline-block;
+width: 60px;
+height: 34px;
+}
+
+/* Hide default HTML checkbox */
+.switch input {
+opacity: 0;
+width: 0;
+height: 0;
+}
+
+/* The slider */
+.slider {
+position: absolute;
+cursor: pointer;
+top: 0;
+left: 0;
+right: 0;
+bottom: 0;
+background-color: #ccc;
+-webkit-transition: .4s;
+transition: .4s;
+}
+
+.slider:before {
+position: absolute;
+content: "";
+height: 26px;
+width: 26px;
+left: 4px;
+bottom: 4px;
+background-color: white;
+-webkit-transition: .4s;
+transition: .4s;
+}
+
+input:checked + .slider {
+background-color: #2196F3;
+}
+
+input:focus + .slider {
+box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked + .slider:before {
+-webkit-transform: translateX(26px);
+-ms-transform: translateX(26px);
+transform: translateX(26px);
+}
+
+/* Rounded sliders */
+.slider.round {
+border-radius: 34px;
+}
+
+.slider.round:before {
+border-radius: 50%;
+}
+</style>
+
 <div class="t1">
   <label> <br>Atividade:<br> </label>
   <input id="grande" type="text" name="atividade" value="{{ isset($registro->atividade) ? $registro->atividade : '' }}">
@@ -50,6 +115,16 @@
   <input id="medio" type="text" name="dataLimiteShow" value="{{ isset($registro->dataLimite) ? $registro->dataLimite : '' }}" disabled>
 </div>
 
+<div class="t3">
+    <div class="widget">
+        <fieldset id="fieldset">
+            <legend>Hotel Ratings: </legend>
+            <label for="status">Status</label>
+            <input type="checkbox" name="status" id="status">
+        </fieldset>
+    </div>
+</div>
+
 <div class="t1">
   <label> <br>Obervações:<br> </label>
   <textarea id="grande" name="observacao" cols="80" rows="7" style="height: 90%">{{ isset($registro->atividade) ? $registro->atividade : '' }}</textarea>
@@ -82,5 +157,11 @@
         $("input[name=dataLimiteShow]").val(dataArr[2] + "-" + dataArr[1] + "-" + dataArr[0])
         $("input[name=dataLimiteShow]").mask('00/00/0000', { reverse : true });
     });
+    </script>
+    <script>
+        $( function() {
+            $( "#status" ).checkboxradio();
+            $( "#fieldset" ).controlgroup();
+        } );
     </script>
 @endsection
